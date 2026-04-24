1b9b1ffd27b64af89ae483fd6d5a42f6061d3a1f
Morino Yimfack <morinoyimfack10@gmail.com>
2026-04-25 01:20:59 +0200

    Update GestionBoutiqueApplicationTests.java

--- gestion_boutique/src/test/java/com/com/gestion_boutique/GestionBoutiqueApplicationTests.java
+++ gestion_boutique/src/test/java/com/com/gestion_boutique/GestionBoutiqueApplicationTests.java
@@ -2,7 +2,7 @@ package com.com.gestion_boutique;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//Test
 @SpringBootTest
 class GestionBoutiqueApplicationTests {
 
